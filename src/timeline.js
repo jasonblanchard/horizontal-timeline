888f28d2070e3970ebd52028b658841ff8e67fa2
Jason Blanchard <jas.blanchard@gmail.com>
2016-02-22 08:27:45 -0500

    Fixed scale

--- src/timeline.js
+++ src/timeline.js
@@ -6,7 +6,7 @@ const data = rawData.sort((a, b) => new Date(a.start) - new Date(b.start));
 export default () => {
   const margins = { top: 20, right: 10, bottom: 0, left: 0 };
   const width = 1160 - margins.right - margins.left;
-  const height = 1000 - margins.top - margins.bottom;
+  const height = 350 - margins.top - margins.bottom;
 
   const chart = d3.select('.chart')
     .attr('width', width + margins.right + margins.left)
@@ -19,8 +19,8 @@ export default () => {
     .range([0, width - margins.left - margins.right]);
 
   const yScale = d3.scale.ordinal()
-    .rangeRoundBands([0, height], 0.1)
-    .domain(data.map(d => d.name));
+    .domain(data.map(d => d.name))
+    .rangeBands([0, height], 0.1);
 
   const xAxis = d3.svg.axis()
     .orient('top')
@@ -36,16 +36,17 @@ export default () => {
     .data(data).enter()
     .append('g')
     .attr('class', 'entry')
-    .attr('transform', (d, i) => `translate(0, ${i * 30})`);
+    .attr('transform', d => `translate(0, ${yScale(d.name)})`);
 
   bar.append('rect')
     .attr('x', d => xScale(new Date(d.start)))
-    .attr('height', yScale.rangeBand() / data.length)
+    // .attr('height', yScale.rangeBand() / data.length)
+    .attr('height', yScale.rangeBand())
     .attr('width', d => xScale(new Date(d.end)) - xScale(new Date(d.start)));
 
   bar.append('text')
-    .attr('x', d => xScale(new Date(d.start)))
-    .attr('y', (yScale.rangeBand() / data.length) / 2)
+    .attr('x', d => xScale(new Date(d.start)) + 5)
+    .attr('y', yScale.rangeBand() / 2)
     .attr('dy', '.35em')
     .text(d => `${d.name} ${new Date(d.start).getFullYear()}-${new Date(d.end).getFullYear()}`);
 };
